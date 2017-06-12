bde8a71381b62f46e7bf99110b79da17a0959312
ShiDacheng <554601845@qq.com>
2017-06-12 08:55:41 +0800

    sef

--- src/script/views/index.js
+++ src/script/views/index.js
@@ -7,3 +7,21 @@ var commonUtil = require('../utils/common')
 commonUtil.renderBody(indexTpl)
 commonUtil.render(document.getElementById('header'), headerTpl)
 commonUtil.render(document.getElementById('footer'), footerTpl)
+
+
+$('#myTemplate').tap(function(e){
+	console.log($(e.target));
+	$(e.target).next().toggle();
+})
+
+
+var myswiper = new Swiper('.swiper-container', {
+    direction: 'horizontal',
+    loop: false ,
+    autoplay:2500,
+    speed:2500,
+    freeMode : true,
+    // 如果需要分页器
+    pagination: '.swiper-pagination'
+})        
+
---
+++ src/script/views/search.js
@@ -0,0 +1,26 @@
+var indexTpl=require('../tpls/search.string')
+var commonUtil=require('../utils/common')
+
+
+commonUtil.renderBody(indexTpl)
+
+var data = {};
+data.list = [];
+$('#search-input').blur(function(){
+	data.list.unshift($(this).val());
+	$('.history2').html(template('historyTel', data));
+	window.location.href="list.html";
+})
+
+$('#failback').click(function(){
+	window.location.href="index.html"
+});
+
+$('#hot-content').click(function(){
+	window.location.href="list.html"
+});
+
+
+
+
+
